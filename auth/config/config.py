ea80ce8381e3b5ef9f6e10e7684b4b53b48295bc
Artyom Danilov <artemdancte@mail.ru>
2024-10-24 19:49:44 +0300

    feet[config:class]: добавил класс для валидации данных для PgSQL

--- auth/config/config.py
+++ auth/config/config.py
@@ -13,3 +13,19 @@ class ServiseSettings(BaseSettings):
         env_file='.env',
         extra='ignore',
     ))
+
+
+class PgSqlSettings(ServiseSettings):
+    """Settings for PgSql.
+
+    Args:
+        ServiseSettings (class): Servise settings
+    """
+
+    db_host: str
+    db_password: str
+    db_port: int
+    db_shema: str
+    db_name: str
+    db_user: str
+    db_driver: str
