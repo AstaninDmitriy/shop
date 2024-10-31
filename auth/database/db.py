fd3f7d1abb3e165ace110f0dd8f7816fd8d8eb46
Artyom Danilov <artemdancte@mail.ru>
2024-10-31 14:08:01 +0300

    fix[db]: исправил баг

--- auth/database/db.py
+++ auth/database/db.py
@@ -3,12 +3,15 @@ from config.config import pgsqlsettings
 
 
 class PostgreSql():
+    """Класс для работы с pgsql."""
+
     def __init__(self):
+        """Init func."""
         self.DSN = '{driver}://{name}:{password}@{host}/{port}:{user}'.format(
             driver=pgsqlsettings.db_driver,
             name=pgsqlsettings.db_name,
             password=pgsqlsettings.db_password,
             host=pgsqlsettings.db_host,
             port=pgsqlsettings.db_port,
-            user=pgsqlsettings.db_user
+            user=pgsqlsettings.db_user,
         )
